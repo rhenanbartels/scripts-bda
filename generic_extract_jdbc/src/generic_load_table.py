6a08a752b4dd2d857134f5fe905ea3370a35892b
samambaman <felipe.gomes.ferreira@gmail.com>
2019-07-03 16:36:28 -0300

    Inclusão do modulo de load somente dos novos registros

--- generic_extract_jdbc/src/generic_load_table.py
+++ generic_extract_jdbc/src/generic_load_table.py
@@ -1,15 +1,15 @@
 from base import spark
 from decouple import config
+from datetime import date
 import params_table
 
 url_oracle_server = config('ORACLE_SERVER')
 user_oracle = config("ORACLE_USER")
 passwd_oracle = config("ORACLE_PASSWORD")
+load_all = config("LOAD_ALL")
 
-config_params = params_table.params
 
-for table in config_params['tables']:
-
+def load_all_data(table):
     #Get minimum and maximum record from table oracle for just used to decide the partition stride
     query_primarykeys = " (select min({key}), max({key}) from {table_oracle}) p ".format(key=table['key_table_oracle'], table_oracle=table['table_oracle'])
     query_table = table['table_oracle']
@@ -37,11 +37,68 @@ for table in config_params['tables']:
     .option("user", user_oracle) \
     .option("password", passwd_oracle) \
     .load()
-    print('Geting all data from table oracle')
+    print('Getting all data from table oracle')
 
     #oracle_table.repartition(20).write.insertInto(tableName=table['table_hive'], overwrite=True)
     oracle_table.repartition(20).write.mode('overwrite').saveAsTable(table['table_hive'])
     print('Inserting data into final table %s' % table['table_hive'])
 
     spark.sql("ANALYZE TABLE {} COMPUTE STATISTICS".format(table['table_hive']))
-    spark.sql("ANALYZE TABLE {} COMPUTE STATISTICS FOR COLUMNS".format(table['table_hive']))
+    #spark.sql("ANALYZE TABLE {} COMPUTE STATISTICS FOR COLUMNS".format(table['table_hive']))
+
+
+def load_part_data(table):
+    
+    spark.read.table(table['table_hive']).createOrReplaceTempView("table_all")
+    spark.catalog.cacheTable("table_all")
+
+    max_key_value = int(spark.sql("select max(%s) from table_all" % table['key_table_oracle']).first()[0])
+    print('Getting max key value from table %s ' % table['table_hive'])
+
+    if table['update_date_table_oracle']:
+        max_date_value = spark.sql("select max(%s) from table_all" % table['update_date_table_oracle']).first()[0].strftime("%Y-%m-%d")
+        condition = """  
+                    or TO_CHAR({update_date_table_oracle},'YYYY-MM-DD') > '{max_date_value}' 
+                    """.format(update_date_table_oracle=table['update_date_table_oracle'], max_date_value=max_date_value)
+        print('Getting max date from table %s and add condition to query' % table['table_hive'])
+    
+    #Get all last data inserted and all data updated in table oracle
+    query =  """
+            (SELECT * FROM {table_oracle} 
+            WHERE {key} > {max_key_value} {condition}) q 
+            """.format(key=table['key_table_oracle'], table_oracle=table['table_oracle'], 
+                max_key_value=max_key_value, condition=condition if table['update_date_table_oracle'] else "")
+
+    spark.read.format("jdbc") \
+        .option("url", url_oracle_server) \
+        .option("dbtable", query) \
+        .option("user", user_oracle) \
+        .option("password", passwd_oracle) \
+        .option("driver", config_params['driver']) \
+        .load().createOrReplaceTempView("table_delta")
+    print('Getting new data from table %s oracle' % table['update_date_table_oracle'])
+
+    #Join the actual data table hive with the updated data
+    update_df = spark.sql("select table_all.* from table_all join table_delta on table_all.{key} = table_delta.{key}".format(key=table['key_table_oracle']))
+    table_all_df = spark.sql("from table_all")
+    table_delta_df = spark.sql("from table_delta")
+
+    total_df = table_all_df.subtract(update_df).union(table_delta_df)
+    print('Update actual data in table hive with new data from table oracle')
+
+    total_df.write.mode("overwrite").saveAsTable("temp_table")
+    temp_table = spark.table("temp_table")
+    temp_table.repartition(20).write.mode('overwrite').saveAsTable(table['table_hive'])
+    print('Writing data in hdfs like table %s ' % table['table_hive'])
+    
+    spark.sql("drop table temp_table")
+    spark.catalog.clearCache()
+
+
+config_params = params_table.params
+
+for table in config_params['tables']:
+    if load_all:
+        load_all_data(table)
+    else:
+        load_part_data(table)
--- generic_extract_jdbc/src/params_table.py
+++ generic_extract_jdbc/src/params_table.py
@@ -1,6 +1,29 @@
 params = {
     "driver" : "oracle.jdbc.driver.OracleDriver",
     "tables" : [
-        { "table_oracle" : "MCPR.MCPR_PERSONAGEM", "key_table_oracle" : "PERS_DOCU_DK", "table_hive" : "exadata_dev.mcpr_personagem"}
+        { "table_oracle" : "MCPR.MCPR_ACOMPANHAMENTO_DOCTO_LOG", "key_table_oracle" : "ACDL_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_acompanhamento_docto_log"},
+        { "table_oracle" : "MCPR.MCPR_ACOMPANHAMENTO_DOCUMENTO", "key_table_oracle" : "ACOM_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_acompanhamento_documento"},
+        { "table_oracle" : "MCPR.MCPR_ACOMPANHAMENTO_REGRA", "key_table_oracle" : "ACRE_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_acompanhamento_regra"},
+        { "table_oracle" : "MCPR.MCPR_ADVOGADO", "key_table_oracle" : "ADGO_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_advogado"},
+        { "table_oracle" : "MCPR.MCPR_ADVOGADO_PERSONAGEM", "key_table_oracle" : "ADPR_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_advogado_personagem"},
+        { "table_oracle" : "MCPR.MCPR_AMBIENTE_AGRESSAO", "key_table_oracle" : "AMAG_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_ambiente_agressao"},
+        { "table_oracle" : "MCPR.MCPR_ANDAMENTO", "key_table_oracle" : "PCAO_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_andamento"},
+        { "table_oracle" : "MCPR.MCPR_ANDAMENTO_FASE_EXCECAO", "key_table_oracle" : "TPEX_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_andamento_fase_excecao"},
+        { "table_oracle" : "MCPR.MCPR_ANDAMENTO_OBRIGATORIO", "key_table_oracle" : "ANOB_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_andamento_obrigatorio"},
+        { "table_oracle" : "MCPR.MCPR_ANDAMENTO_PARCIAL", "key_table_oracle" : "PCAO_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_andamento_parcial"},
+        { "table_oracle" : "MCPR.MCPR_ANEXO_PRESTACAO_CONTA", "key_table_oracle" : "MAPC_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_anexo_prestacao_conta"},
+        { "table_oracle" : "MCPR.MCPR_ANOTACAO_DOCUMENTO", "key_table_oracle" : "ANTD_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_anotacao_documento"},
+        { "table_oracle" : "MCPR.MCPR_ANUARIO", "key_table_oracle" : "ANUA_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_anuario"},
+        { "table_oracle" : "MCPR.MCPR_ANUARIO_ESTATISTICA", "key_table_oracle" : "ANES_ORGI_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_anuario_estatistica"},
+        { "table_oracle" : "MCPR.MCPR_ARQUIVAMENTO", "key_table_oracle" : "MARQ_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_arquivamento"},
+        { "table_oracle" : "MCPR.MCPR_ARTIGO", "key_table_oracle" : "ARTG_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_artigo"},
+        { "table_oracle" : "MCPR.MCPR_ASSUNTO", "key_table_oracle" : "ASSU_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_assunto"},
+        { "table_oracle" : "MCPR.MCPR_ASSUNTOS_DE_ATENDIMENTO", "key_table_oracle" : "ADAT_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_assuntos_de_atendimento"},
+        { "table_oracle" : "MCPR.MCPR_ASSUNTO_ATENDIMENTO", "key_table_oracle" : "ASAT_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_assunto_atendimento"},
+        { "table_oracle" : "MCPR.MCPR_ASSUNTO_DOCTO_CONTR_INTEG", "key_table_oracle" : "ARLT_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_assunto_docto_contr_integ"},
+        { "table_oracle" : "MCPR.MCPR_ASSUNTO_DOCUMENTO", "key_table_oracle" : "ASDO_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_assunto_documento"},
+        { "table_oracle" : "MCPR.MCPR_ATENDIMENTO", "key_table_oracle" : "ATND_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_atendimento"},
+        { "table_oracle" : "MCPR.MCPR_AVISO_APLICACAO", "key_table_oracle" : "AISO_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_aviso_aplicacao"},
+        { "table_oracle" : "MCPR.MCPR_PERSONAGEM", "key_table_oracle" : "PERS_DK", "update_date_table_oracle" : "", "table_hive" : "exadata.mcpr_personagem"}
     ]
 }
