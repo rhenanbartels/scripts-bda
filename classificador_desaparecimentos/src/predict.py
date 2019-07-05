4f8036ea149b82535f8312174b8e35a29d4d7c1d
Berne023 <bernardoccordeiro@gmail.com>
2019-07-05 13:55:24 -0300

    Tested in correcao database, first seemingly working version

--- classificador_desaparecimentos/src/predict.py
+++ classificador_desaparecimentos/src/predict.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
 import pickle
@@ -11,13 +12,14 @@ URL_ORACLE_SERVER = config('URL_ORACLE_SERVER')
 USER_ORACLE = config('USER_ORACLE')
 PASSWD_ORACLE = config('PASSWD_ORACLE')
 ORACLE_DRIVER_PATH = config('ORACLE_DRIVER_PATH')
+ROBOT_NAME = config('ROBOT_NAME')
+ROBOT_NUMBER = config('ROBOT_NUMBER')
 HDFS_URL = config('HDFS_URL')
 HDFS_USER = config('HDFS_USER')
 HDFS_MODEL_DIR = config('HDFS_MODEL_DIR')
 
 client = InsecureClient(HDFS_URL, user=HDFS_USER)
 
-NEGATIVE_CLASS_VALUE = 13
 ID_COLUMN = 'SNCA_DK'
 TEXT_COLUMN = 'SNCA_DS_FATO'
 LABEL_COLUMN = 'MDEC_DK'
@@ -37,9 +39,8 @@ QUERY = """
 SELECT DISTINCT B.SNCA_DK, B.SNCA_DS_FATO, D.DMDE_MDEC_DK
 FROM SILD.SILD_ATIVIDADE_SINDICANCIA A
 INNER JOIN SILD.SILD_SINDICANCIA B ON A.ATSD_SNCA_DK = B.SNCA_DK
-INNER JOIN CORP.CORP_UF C ON C.UFED_DK = B.SNCA_UFED_DK
 INNER JOIN SILD.SILD_DESAPARE_MOT_DECLARADO D ON D.DMDE_SDES_DK = B.SNCA_DK
-WHERE A.ATSD_TPSN_DK = 2 AND C.UFED_SIGLA = 'RJ'
+WHERE A.ATSD_TPSN_DK = 2 AND B.SNCA_UFED_DK = 33
 AND NOT EXISTS (SELECT ATSD_SNCA_DK 
 FROM SILD.SILD_ATIVIDADE_SINDICANCIA B WHERE B.ATSD_SNCA_DK = A.ATSD_SNCA_DK AND B.ATSD_TPSN_DK = 22)
 """
@@ -90,14 +91,38 @@ df_results = pd.DataFrame(
     columns=[ID_COLUMN, LABEL_COLUMN]
 )
 
+# Should only commit everything at the end, in a single transaction
+conn.jconn.setAutoCommit(False)
+
+curs.execute("CALL dbms_application_info.set_module(?, ?)", ('SILD', 'Funcionalidade'))
+curs.execute("CALL dbms_application_info.set_client_info(?)", ('modelo_ia_teste',))
+curs.execute("SELECT MAX(ATSD_DK) FROM SILD.SILD_ATIVIDADE_SINDICANCIA")
+max_atsd_dk = int(curs.fetchall()[0][0]))
+
+ATIV_SINDICANCIA_QUERY = """
+    INSERT INTO SILD.SILD_ATIVIDADE_SINDICANCIA
+    (ATSD_DK, ATSD_SNCA_DK, ATSD_TPSN_DK, ATSD_DT_REGISTRO, ATSD_DS_MOTIVO_ATIVIDADE, ATSD_NM_RESP_CTRL, ATSD_CPF_RESP_CTRL)
+    VALUES (?, ?, 23, SYSDATE, 'CLASSIFICAÇÃO FEITA PELO ROBÔ', ?, ?)
+"""
+
+DELETE_MOT_DECLARADO_QUERY = """
+    DELETE FROM SILD.SILD_DESAPARE_MOT_DECLARADO WHERE DMDE_SDES_DK = ?
+"""
+
+INSERT_MOT_DECLARADO_QUERY = """
+    INSERT INTO SILD.SILD_DESAPARE_MOT_DECLARADO (DMDE_SDES_DK, DMDE_MDEC_DK) VALUES (?, ?)
+"""
+
 print('Writing results to tables')
-for labels, dk in zip(df_results[LABEL_COLUMN].values, df_results[ID_COLUMN]):
-    break
-    #cur.execute("INSERT INTO SILD.SILD_ATIVIDADE_SINDICANCIA WHERE DMDE_SDES_DK = ?", (int(dk),))
+for labels, snca_dk in zip(df_results[LABEL_COLUMN].values, df_results[ID_COLUMN]):
+
+    max_atsd_dk += 1
+    curs.execute(ATIV_SINDICANCIA_QUERY, (max_atsd_dk, int(snca_dk), ROBOT_NAME, ROBOT_NUMBER))
     
-    #cur.execute("DELETE FROM SILD.SILD_DESAPARE_MOT_DECLARADO WHERE DMDE_SDES_DK = ?", (int(dk),))
-    #for label in labels: 
-    #    cur.execute("INSERT INTO SILD.SILD_DESAPARE_MOT_DECLARADO (DMDE_SDES_DK, DMDE_MDEC_DK) VALUES (?, ?)", (int(dk), int(label)))
-    
-# TODO: Check correct way to commit using JayDeBeApi 
-#conn.commit() 
+    curs.execute(DELETE_MOT_DECLARADO_QUERY, (int(snca_dk),))
+    for label in labels: 
+        curs.execute(INSERT_MOT_DECLARADO_QUERY, (int(snca_dk), int(label)))
+
+conn.commit()
+curs.close()
+conn.close() 
