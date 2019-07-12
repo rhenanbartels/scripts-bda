09c3cd69fa4b4834433ac7bd4e455a5e00dc8b39
Berne023 <bernardoccordeiro@gmail.com>
2019-07-12 12:28:44 -0300

    run_classifier command now echoes an error message with an invalid command

--- classificador_desaparecimentos/run_classifier.sh
+++ classificador_desaparecimentos/run_classifier.sh
@@ -12,8 +12,10 @@ command="${1}"
 
 if [ ${command} = 'test' ]; then
     /opt/cloudera/parcels/Anaconda-5.0.1/bin/python src/run_tests.py
-else
+elif [ ${command} = 'predict' -o ${command} = 'train' ]; then
     spark2-submit --py-files packages/*.whl,packages/*.egg \
         --jars /opt/cloudera/parcels/CDH-5.14.2-1.cdh5.14.2.p0.3/jars/ojdbc6.jar \
         src/${command}.py 2>> error.log
+else
+    echo "command must be either \"predict\", \"train\" or \"test\""
 fi
