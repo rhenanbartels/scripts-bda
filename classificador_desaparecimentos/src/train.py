7d13f0ab072c92bc5b9fec232c04ef66eeb26aa8
Berne023 <bernardoccordeiro@gmail.com>
2019-07-24 18:25:00 -0300

    Train phase now gets list of classes before fitting dataset

--- classificador_desaparecimentos/src/train.py
+++ classificador_desaparecimentos/src/train.py
@@ -13,7 +13,10 @@ from utils import (
     get_keys,
     OneVsRestLogisticRegression
 )
-from queries import get_train_data
+from queries import (
+    get_train_data,
+    get_list_of_classes
+)
 
 
 URL_ORACLE_SERVER = config('URL_ORACLE_SERVER')
@@ -23,17 +26,17 @@ ORACLE_DRIVER_PATH = config('ORACLE_DRIVER_PATH')
 HDFS_URL = config('HDFS_URL')
 HDFS_USER = config('HDFS_USER')
 HDFS_MODEL_DIR = config('HDFS_MODEL_DIR')
-START_DATE = config('START_DATE')
+START_DATE = config('START_DATE', default=None)
+UFED_DK = config('UFED_DK', default=None)
 
 NEGATIVE_CLASS_VALUE = 13
 ID_COLUMN = 'SNCA_DK'
 TEXT_COLUMN = 'SNCA_DS_FATO'
 LABEL_COLUMN = 'DMDE_MDEC_DK'
-# 33 = RJ
-UFED_DK = 33
+
 NGRAM_RANGE = (1, 3)
 MAX_DF = 0.6
-MIN_DF = 5
+MIN_DF = 1
 
 print('Running train script:')
 print('Querying database...')
@@ -45,6 +48,7 @@ conn = jdbc.connect("oracle.jdbc.driver.OracleDriver",
 curs = conn.cursor()
 
 df = get_train_data(curs, UFED_DK=UFED_DK, start_date=START_DATE)
+
 train_keys = get_keys(df, ID_COLUMN)
 
 print('Preparing data...')
@@ -54,7 +58,8 @@ df = df.groupby(TEXT_COLUMN)\
        .agg(lambda x: set(x))\
        .reset_index()
 
-mlb = MultiLabelBinarizer()
+classes = get_list_of_classes(curs)
+mlb = MultiLabelBinarizer(classes)
 y = df[LABEL_COLUMN]
 y = mlb.fit_transform(y)
 
