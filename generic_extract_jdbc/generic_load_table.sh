9287e1ebc6a89aceda2b4990f4d0949adf8ee887
matteushc <matteus_h@hotmail.com>
2019-07-25 14:36:52 -0300

    Ajustando a funcao load_part_data para dar somente append na tabela

--- generic_extract_jdbc/generic_load_table.sh
+++ generic_extract_jdbc/generic_load_table.sh
@@ -8,5 +8,6 @@ spark2-submit \
     --executor-memory 22g \
     --conf spark.debug.maxToStringFields=2000 \
     --conf spark.executor.memoryOverhead=4096 \
+    --conf spark.network.timeout=300 \
     --py-files src/timer.py,src/base.py,packages/*.whl,packages/*.egg \
     src/generic_load_table.py 2>> error.log
