128570bce5784a7d8d603416aa8384d51f93695d
matteushc <matteus_h@hotmail.com>
2019-08-13 12:05:50 -0300

    Alterando as variaveis de conexao com o servidor jdbc para ser mais generico

--- generic_extract_jdbc/src/generic_load_table.py
+++ generic_extract_jdbc/src/generic_load_table.py
@@ -7,22 +7,22 @@ import params_table_postgre
 import pyspark.sql.functions as f
 from pyspark.sql.functions import base64
 
-url_oracle_server = config('ORACLE_SERVER')
-user_oracle = config("ORACLE_USER")
-passwd_oracle = config("ORACLE_PASSWORD")
+url_jdbc_server = config('JDBC_SERVER')
+user_jdbc = config("JDBC_USER")
+passwd_jdbc = config("JDBC_PASSWORD")
 load_all = config("LOAD_ALL")
 
 
 def load_all_data(table):
     """
-    Method for load all data coming from oracle table
+    Method for load all data coming from jdbc table
 
     Parameters
     ----------
     table: dict
-        "table_oracle" : oracle table name
-        "pk_table_oracle" : primary key oracle table
-        "update_date_table_oracle" : update date oracle table
+        "table_jdbc" : jdbc table name
+        "pk_table_jdbc" : primary key jdbc table
+        "update_date_table_jdbc" : update date jdbc table
         "table_hive" : 	hive table name
         "fields"
         (
@@ -33,30 +33,30 @@ def load_all_data(table):
 
     print("Start process load all")
     # Get minimum and maximum record
-    # from table oracle for just used to decide the partition stride
+    # from table jdbc for just used to decide the partition stride
     query_primarykeys = """
             (select
             count(1) as total,
             min({key}),
             max({key})
-            from {table_oracle}) p """.format(
-                key=table['pk_table_oracle'],
-                table_oracle=table['table_oracle'])
+            from {table_jdbc}) p """.format(
+                key=table['pk_table_jdbc'],
+                table_jdbc=table['table_jdbc'])
 
     if table.get('fields'):
-        query_table = """(SELECT {fields} FROM {table_oracle}) q """.format(
+        query_table = """(SELECT {fields} FROM {table_jdbc}) q """.format(
             fields=table['fields'],
-            table_oracle=table['table_oracle'])
+            table_jdbc=table['table_jdbc'])
     else:
-        query_table = table['table_oracle']
+        query_table = table['table_jdbc']
 
-    print('Geting min and max from table %s oracle' % table['table_oracle'])
+    print('Geting min and max from table %s jdbc' % table['table_jdbc'])
     
     total_min_max_table = spark.read.format("jdbc") \
-        .option("url", url_oracle_server) \
+        .option("url", url_jdbc_server) \
         .option("dbtable", query_primarykeys) \
-        .option("user", user_oracle) \
-        .option("password", passwd_oracle) \
+        .option("user", user_jdbc) \
+        .option("password", passwd_jdbc) \
         .option("driver", config_params['driver']) \
         .load()
 
@@ -67,16 +67,16 @@ def load_all_data(table):
         minimum = int(total_min_max_table.first()[1])
         maximum = int(total_min_max_table.first()[2])
 
-        print('Getting all data from table %s oracle' % table['table_oracle'])
-        oracle_table = spark.read.format("jdbc") \
-            .option("url", url_oracle_server) \
+        print('Getting all data from table %s jdbc' % table['table_jdbc'])
+        jdbc_table = spark.read.format("jdbc") \
+            .option("url", url_jdbc_server) \
             .option("lowerBound", minimum) \
             .option("upperBound", maximum) \
             .option("numPartitions", 50) \
-            .option("partitionColumn", table['pk_table_oracle']) \
+            .option("partitionColumn", table['pk_table_jdbc']) \
             .option("dbtable", query_table) \
-            .option("user", user_oracle) \
-            .option("password", passwd_oracle) \
+            .option("user", user_jdbc) \
+            .option("password", passwd_jdbc) \
             .option("driver", config_params['driver']) \
             .load()
 
@@ -85,7 +85,7 @@ def load_all_data(table):
 
         print('Inserting data into final table %s' % table_hive)
 
-        final_df = transform_col_binary(oracle_table)
+        final_df = transform_col_binary(jdbc_table)
 
         final_df.coalesce(20) \
             .write \
@@ -100,14 +100,14 @@ def load_all_data(table):
 
 def load_part_data(table):
     """
-    Method for load just the new data or updated data coming from oracle table
+    Method for load just the new data or updated data coming from jdbc table
 
     Parameters
     ----------
     table: dict
-        "table_oracle" : oracle table name
-        "pk_table_oracle" : primary key oracle table
-        "update_date_table_oracle" : update date oracle table
+        "table_jdbc" : jdbc table name
+        "pk_table_jdbc" : primary key jdbc table
+        "update_date_table_jdbc" : update date jdbc table
         "table_hive" : hive table name
         "fields"
         (
@@ -132,13 +132,13 @@ def load_part_data(table):
 
         # Get count and max from hive table.
         # Count for check if table has data and max
-        # for check the new data from oracle table
+        # for check the new data from jdbc table
         total_max_table = spark \
             .sql("""
                 select count(1) as total,
                 max({})
                 from {}
-                """.format(table['pk_table_oracle'], table_hive))
+                """.format(table['pk_table_jdbc'], table_hive))
 
         total = total_max_table.first()[0]
 
@@ -147,20 +147,20 @@ def load_part_data(table):
             max_key_value = int(total_max_table.first()[1])
             print('Getting max key value from table %s ' % table_hive)
 
-            # If parameter update_date_table_oracle
+            # If parameter update_date_table_jdbc
             # exist get max update date from
-            # hive table to retrive updated data from oracle table
-            if table['update_date_table_oracle']:
+            # hive table to retrive updated data from jdbc table
+            if table['update_date_table_jdbc']:
                 max_date_value = spark.sql("""
                     select max(%s)
-                    from table_all """ % table['update_date_table_oracle']) \
+                    from table_all """ % table['update_date_table_jdbc']) \
                         .first()[0].strftime("%Y-%m-%d")
 
                 condition = """
-                or TO_CHAR({update_date_table_oracle},'YYYY-MM-DD')
+                or TO_CHAR({update_date_table_jdbc},'YYYY-MM-DD')
                 > '{max_date_value}'
                 """.format(
-                    update_date_table_oracle=table['update_date_table_oracle'],
+                    update_date_table_jdbc=table['update_date_table_jdbc'],
                     max_date_value=max_date_value)
 
                 print("""
@@ -168,27 +168,27 @@ def load_part_data(table):
                 table %s and add condition to query
                 """ % table_hive)
 
-            # Get all last data inserted and all data updated in table oracle
+            # Get all last data inserted and all data updated in table jdbc
             query = """
-                    (SELECT {fields} FROM {table_oracle}
+                    (SELECT {fields} FROM {table_jdbc}
                     WHERE {key} > {max_key_value} {condition}) q
                     """.format(
-                        key=table['pk_table_oracle'],
-                        table_oracle=table['table_oracle'],
+                        key=table['pk_table_jdbc'],
+                        table_jdbc=table['table_jdbc'],
                         max_key_value=max_key_value,
                         fields=table['fields'] if table.get('fields') else "*",
                         condition=condition if
-                        table['update_date_table_oracle'] else "")
+                        table['update_date_table_jdbc'] else "")
 
             print("""
                 Getting new data
-                from table %s oracle """ % table['table_oracle'])
+                from table %s jdbc """ % table['table_jdbc'])
 
             spark.read.format("jdbc") \
-                .option("url", url_oracle_server) \
+                .option("url", url_jdbc_server) \
                 .option("dbtable", query) \
-                .option("user", user_oracle) \
-                .option("password", passwd_oracle) \
+                .option("user", user_jdbc) \
+                .option("password", passwd_jdbc) \
                 .option("driver", config_params['driver']) \
                 .load().createOrReplaceTempView("table_delta")
 
@@ -203,14 +203,14 @@ def load_part_data(table):
                 #from table_all
                 #join table_delta
                 #on table_all.{key} = table_delta.{key} """.format(
-                #    key=table['pk_table_oracle']))
+                #    key=table['pk_table_jdbc']))
 
                 #table_all_df = spark.sql("from table_all")
                 table_delta_df = spark.sql("from table_delta")
 
                 print("""
                 Update actual data in table
-                hive with new data from table oracle
+                hive with new data from table jdbc
                 """)
 
                 #total_df = table_all_df \
--- generic_extract_jdbc/src/params_table_postgre.py
+++ generic_extract_jdbc/src/params_table_postgre.py
@@ -4,10 +4,22 @@ params = {
     "tables": [
         # Schema MCPR
         {
-            "table_oracle": "financeiro.financeiro",
-            "pk_table_oracle": "ID",
-            "update_date_table_oracle": "",
+            "table_jdbc": "financeiro.financeiro",
+            "pk_table_jdbc": "ID",
+            "update_date_table_jdbc": "",
             "table_hive": "financeiro"
+        },
+        {
+            "table_jdbc": "(SELECT nome, cpf, localidade, cod_ibge::integer, ano_eleicao, foto FROM lupa.governantes_rj) t",
+            "pk_table_jdbc": "cod_ibge",
+            "update_date_table_jdbc": "",
+            "table_hive": "lupa_governantes_rj"
+        },
+        {
+            "table_jdbc": "(SELECT cod_craai, nome_craai, municipio,  cod_municipio::integer, gentilico, prefeito, vice_prefeito, site, telefone, endereco, fonte FROM lupa.prefeituras) t",
+            "pk_table_jdbc": "cod_municipio",
+            "update_date_table_jdbc": "",
+            "table_hive": "lupa_prefeituras"
         }
     ]
 }
