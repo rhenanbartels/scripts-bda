d9bdff87ef39492edbe07cab437e99743f456f76
matteushc <matteus_h@hotmail.com>
2019-08-13 12:04:44 -0300

    Adicionando o python enconding

--- generic_extract_jdbc/generic_load_table.sh
+++ generic_extract_jdbc/generic_load_table.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export PYTHONIOENCODING=utf8
 
 spark2-submit \
     --jars /opt/cloudera/parcels/CDH-5.14.2-1.cdh5.14.2.p0.3/jars/ojdbc6.jar,/opt/cloudera/parcels/CDH-5.14.2-1.cdh5.14.2.p0.3/jars/postgresql-9.0-801.jdbc4.jar \
