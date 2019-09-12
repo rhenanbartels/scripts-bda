b3c418c82b8139adb0c8f66d31548299f5ba3fcb
Berne023 <bernardoccordeiro@gmail.com>
2019-09-12 17:05:06 -0300

    Gets results from all keys classified by Dunant and builds a table for BI team

--- classificador_desaparecimentos/src/evaluate.py
+++ classificador_desaparecimentos/src/evaluate.py
@@ -3,6 +3,7 @@ import sys
 import pickle
 
 import jaydebeapi as jdbc
+import pandas as pd
 from decouple import config
 from hdfs import InsecureClient
 
@@ -42,56 +43,41 @@ conn = jdbc.connect("oracle.jdbc.driver.OracleDriver",
                     ORACLE_DRIVER_PATH)
 curs = conn.cursor()
 
-print('Retrieving prediction results from HDFS...')
-data_hdfs = get_results_from_hdfs(client, FORMATTED_HDFS_PATH)
-# Results are stored as a tuple represented as a string
-data_hdfs['MDEC_DK'] = data_hdfs['MDEC_DK'].apply(
-    lambda x: ast.literal_eval(x))
+model_dates = sorted(client.list(FORMATTED_HDFS_PATH))
+validated_datasets = []
+classified_datasets = []
 
-keys = get_keys(data_hdfs, 'SNCA_DK')
+for model_date in model_dates:
+    try:
+        data_hdfs = get_results_from_hdfs(client, FORMATTED_HDFS_PATH, model_date=model_date)
+    except:
+        continue
+    # Results are stored as a tuple represented as a string
+    data_hdfs['MDEC_DK'] = data_hdfs['MDEC_DK'].apply(
+        lambda x: ast.literal_eval(x))
 
-print('Retrieving data from Oracle...')
-# Only needs the keys that are in the predictions
-data_oracle = get_evaluate_data(curs, keys)
-if len(data_oracle) == 0:
-    sys.exit('No validated documents available!')
+    keys = get_keys(data_hdfs, 'SNCA_DK')
 
-print('Generating classification report...')
-most_recent_date = sorted(client.list(FORMATTED_HDFS_PATH))[-1]
-with client.read('{}/{}/model/mlb_binarizer.pkl'.format(
-        FORMATTED_HDFS_PATH, most_recent_date)) as mlb_reader:
-    mlb = pickle.loads(mlb_reader.read())
+    # Only needs the keys that are in the predictions
+    data_oracle = get_evaluate_data(curs, keys)
 
-report = generate_report(data_hdfs, data_oracle, mlb)
+    data_hdfs = expand_results(data_hdfs)
+    data_hdfs['DT_MODELO'] = model_date
+    data_oracle['DT_MODELO'] = model_date
+    data_oracle['IS_VALIDATION'] = True
+    data_hdfs['IS_VALIDATION'] = False
 
-print('Preparing HDFS data for next metrics...')
-data_hdfs = expand_results(data_hdfs)
+    validated_datasets.append(data_oracle)
+    classified_datasets.append(data_hdfs)
 
-# To evaluate the predictions, only those keys that were effectively validated
-# should be taken into consideration
-data_hdfs = data_hdfs[data_hdfs['SNCA_DK'].isin(
-    data_oracle['SNCA_DK'].unique().tolist())]
 
-print('Calculating evaluation metrics...')
-nb_documents_predicted = len(keys)
-nb_documents_validated = len(get_keys(data_oracle, 'SNCA_DK'))
+df1 = pd.concat(validated_datasets, ignore_index=True)
+df2 = pd.concat(classified_datasets, ignore_index=True)
 
-pctg_change = get_percentage_of_change(data_hdfs, data_oracle)
+result_df = pd.concat([df1, df2], ignore_index=True)
 
-nb_remove, nb_add, nb_swaps = get_number_of_modifications(
-    data_hdfs, data_oracle)
-nb_modifications = nb_remove + nb_add + nb_swaps
-nb_final_classes = len(data_oracle)
+result_df.to_csv('results_dunant.csv', index=False)
 
-print('Saving metrics to HDFS...')
-save_metrics_to_hdfs(
-    client, FORMATTED_HDFS_PATH,
-    docs_predicted=nb_documents_predicted,
-    docs_validated=nb_documents_validated,
-    pctg_change=pctg_change,
-    removals=nb_remove,
-    additions=nb_add,
-    swaps=nb_swaps,
-    modifications=nb_modifications,
-    total_predictions=nb_final_classes,
-    classification_report=report)
+#print(result_df.groupby(['SNCA_DK', 'IS_VALIDATION']).count())
+
+
