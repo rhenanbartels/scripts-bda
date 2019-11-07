72e50c2c3d6a9034b0d664026bec6a3428831d62
matteushc <matteus_h@hotmail.com>
2019-11-07 17:07:32 -0200

    Tratamento de erro ao tentar criar o egg, criando alternativamente um zip

--- generic_extract_jdbc/despython.sh
+++ generic_extract_jdbc/despython.sh
@@ -2,4 +2,10 @@ echo $1
 direc=$1
 cd $direc
 python setup.py bdist_egg
-mv dist/*.egg ../
+if [[ $? == 0 ]]; then
+    mv dist/*.egg ../
+else
+    cd ..
+    folder_name=$(basename "$direc" )
+    zip -r "$folder_name.zip" $direc
+fi
