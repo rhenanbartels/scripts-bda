7dc5b9f55ba97102cccff965ac8dc232d7d52ca3
matteushc <matteus_h@hotmail.com>
2020-01-27 16:11:01 -0200

    Inclusão do processo de recuperação dos dados de placas do Detran no Solr

---
+++ load_files_stream/src/diff_bases_placas_detran_by_data.py
@@ -0,0 +1,32 @@
+from pyspark.sql.functions import col, lit, when
+from pyspark.sql.functions import udf
+from opg.base import spark_session
+from opg.opg_utils import uuidsha, update_uuid
+from opg.timer import timer
+
+spark = spark_session('teste')
+
+uuidshaudf = spark.udf.register('uuidshaudf', uuidsha)
+
+bases_dev_p = spark.table("bases_dev.pcivil_placas")
+data_min = bases_dev_p.select("datapassagem").agg({"datapassagem": "min"}).first()[0]
+data_min = data_min.strftime("%Y-%m-%d %H:%M")
+
+df = spark.table('staging.pcivil_placas')
+
+df = df.where("""datapassagem is null or 
+    datapassagem <= cast(from_unixtime(unix_timestamp('{}', 'yyyy-MM-dd HH:mm'), 'yyyy-MM-dd HH:mm:ss') as timestamp)""".format(data_min))
+
+df = df.withColumn('uuid',
+            uuidshaudf(
+                col('placa'),
+                col('datapassagem').cast('string'),
+                col('lat').cast('string'),
+                col('lon').cast('string')
+            )
+)
+
+df.write.mode("overwrite").format("parquet").saveAsTable("bases_dev.detran_regcivil_teste")
+
+df_2 = spark.table("bases_dev.detran_regcivil_teste")
+df_2.write.mode('append').saveAsTable("bases_dev.pcivil_placas")
---
+++ load_files_stream/src/pyspark_stream_to_solr.py
@@ -0,0 +1,79 @@
+from __future__ import print_function
+import pyspark
+from pyspark import SparkContext, SparkConf
+from pyspark.streaming import StreamingContext
+from decouple import config
+import hashlib
+import datetime
+import sys
+import pysolr
+
+zookeeper_server = config('ZOOKEEPER_SERVER')
+folder_hdfs = config("FOLDER_HDFS")
+checkpoint_hdfs = config("CHECKPOINT_HDFS")
+
+def getSparkSessionInstance(sparkConf):
+    if ("sparkSessionSingletonInstance" not in globals()):
+        globals()["sparkSessionSingletonInstance"] = pyspark.sql.session.SparkSession \
+            .builder \
+            .config(conf=sparkConf) \
+            .getOrCreate()
+    return globals()["sparkSessionSingletonInstance"]
+
+def format_date(dt):
+    return dt.replace(" ", "T") + "Z"
+
+def connect_to_solr():
+    zookeeper = pysolr.ZooKeeper(zookeeper_server)
+    return pysolr.SolrCloud(zookeeper, "files_detran", always_commit=True, timeout=300)
+
+def send_data_to_solr(row):
+    solr = connect_to_solr()
+    solr.add([row], commit=True, overwrite=True)
+
+def generate_uuid(row):
+    return hashlib.sha1(reduce(lambda x, y: x+ y, row)).hexdigest()
+
+def check_row(row, header_list):
+    data = dict(zip(tuple(header_list), row.split(',')))
+    data['data'] = format_date(data['data'])
+    data['uuid'] = generate_uuid(row.split(','))
+    send_data_to_solr(data)
+    return data
+
+def verify_rdd(rdd, folder):
+
+    if not rdd.isEmpty():
+
+        spark = getSparkSessionInstance(rdd.context.getConf())
+
+        header = 'num_camera,placa,lat,long,data,velocidade,faixa'
+        header_list = header.split(',')
+        rdd.filter(lambda r: r.lower() not in header).map(lambda w: check_row(w, header_list)).count()
+
+
+def functionToCreateContext(folder):
+    sparkConf = SparkConf()
+
+    sparkConf.set("spark.dynamicAllocation.enabled", "false")
+    sparkConf.set("spark.streaming.receiver.writeAheadLog.enable", "true")
+
+    sc = SparkContext(appName="SPARK_STREAM_DETRAN_PLACAS",conf=sparkConf)
+
+    sparkStreamingContext = StreamingContext(sc, 60)
+    directoryStream = sparkStreamingContext.textFileStream("{}{}".format(folder_hdfs, folder))
+    directoryStream.foreachRDD(lambda rdd: verify_rdd(rdd, folder))
+
+    return sparkStreamingContext
+
+
+if __name__ == "__main__":
+
+    folder = sys.argv[1]
+
+    checkpoint = '{}{}/'.format(checkpoint_hdfs, folder)
+
+    ssc = StreamingContext.getOrCreate(checkpoint, lambda: functionToCreateContext(folder))
+    ssc.start()
+    ssc.awaitTermination()
+    ssc.stop(False, False)
