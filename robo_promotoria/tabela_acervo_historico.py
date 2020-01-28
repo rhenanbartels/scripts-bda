a2dd86b958e47d13a5c93b2a034a49112e09d13b
matteushc <matteus_h@hotmail.com>
2020-01-28 12:52:39 -0200

    Retirando a data da tabela gerada

--- robo_promotoria/tabela_acervo_historico.py
+++ robo_promotoria/tabela_acervo_historico.py
@@ -13,7 +13,6 @@ table = spark.sql("""
         SELECT * from exadata_aux.tb_acervo_diario
 """)
 
-table.withColumn("data", from_unixtime(unix_timestamp(current_timestamp(), 'yyyy-MM-dd'), 'yyyy-MM-dd').cast('timestamp')) \
-    .write.mode("append").format("parquet").saveAsTable("exadata_aux.tb_acervo_historico")
+table.write.mode("append").format("parquet").saveAsTable("exadata_aux.tb_acervo_historico")
 
 _update_impala_table("exadata_aux.tb_acervo_historico")
