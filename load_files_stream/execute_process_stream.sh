2e5045d9462d2f7aa97cbe540f22be53a8c4c99f
matteushc <matteuscunhas@gmail.com>
2020-03-03 17:27:17 -0300

    Inclusão das variaveis pelo argparse para execucao no cluster

--- load_files_stream/execute_process_stream.sh
+++ load_files_stream/execute_process_stream.sh
@@ -1,1 +1,17 @@
-spark2-submit --py-files packages/*.whl,packages/*.egg src/pyspark_stream_to_solr.py kopp &
+spark2-submit --master yarn --deploy-mode cluster \
+    --num-executors 5 \
+    --executor-memory 3g \
+    --driver-memory 3G \
+    --conf spark.executor.memoryOverhead=4096 \
+    --conf spark.yarn.maxAppAttempts=8 \
+    --conf spark.yarn.am.attemptFailuresValidityInterval=1h \
+    --conf spark.yarn.max.executor.failures=200 \
+    --conf spark.yarn.executor.failuresValidityInterval=1h \
+    --conf spark.task.maxFailures=20 \
+    --conf spark.speculation=true \
+    --conf spark.hadoop.fs.hdfs.impl.disable.cache=true \
+    --queue root.stream \
+    --py-files packages/*.whl,packages/*.egg src/pyspark_stream_to_solr.py \
+        -f kopp -p /user/mpmapas/staging/detranbarreirasplacas/ \
+        -c hdfs:///user/mpmapas/checkpoints_spark/ \
+        -z bda1node05:2181,bda1node06:2181,bda1node07:2181/solr &
