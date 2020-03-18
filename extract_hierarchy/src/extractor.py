2dac23a64c08811a22e3fac0fe3b20d06d0961d8
matteushc <matteuscunhas@gmail.com>
2020-03-18 13:50:39 -0300

    Removendo decouple e adicionando argparse para passagem de parametros

--- extract_hierarchy/src/extractor.py
+++ extract_hierarchy/src/extractor.py
@@ -1,12 +1,11 @@
 #-*-coding:utf-8-*-
 from pyspark.sql.types import IntegerType
 from pyspark.sql.functions import *
-from decouple import config
+from utils import _update_impala_table
+import argparse
 
 import pyspark
 
-schema_exadata = config('SCHEMA_EXADATA')
-schema_exadata_aux = config('SCHEMA_EXADATA_AUX')
 
 def get_descendants(line, table):
     result = []
@@ -25,7 +24,7 @@ def get_hierarchy(line, table):
     return result
 
 
-def create_hierarchical_table(dataframe, table_name):
+def create_hierarchical_table(spark, dataframe, table_name):
     for line in dataframe:
         line['ID'] = int(line['ID'])
         line['ID_PAI'] = int(line['ID_PAI']) if line['ID_PAI'] else None
@@ -33,40 +32,66 @@ def create_hierarchical_table(dataframe, table_name):
         line['HIERARQUIA'] = get_hierarchy(line, dataframe)
 
     table_df = spark.createDataFrame(dataframe)
-    schema_name = "{}.{}".format(schema_exadata_aux, table_name)
-    table_df.coalesce(20).write.format('parquet').saveAsTable(schema_name, mode='overwrite')
+    table_df.coalesce(20).write.format('parquet').saveAsTable(table_name, mode='overwrite')
+
+def execute_process(options):
+
+    spark = pyspark.sql.session.SparkSession\
+            .builder\
+            .appName("tabelas_dominio")\
+            .enableHiveSupport()\
+            .getOrCreate()
+
+    sc = spark.sparkContext
+
+    schema_exadata = options['schema_exadata']
+    schema_exadata_aux = options['schema_exadata_aux']
+
+    andamentos = map(
+        lambda row: row.asDict(),
+        spark.table("{}.mcpr_tp_andamento".format(schema_exadata)).select(
+            col('TPPR_DK').alias('ID'),
+            col('TPPR_DESCRICAO').alias('DESCRICAO'),
+            col('TPPR_CD_TP_ANDAMENTO').alias('COD_MGP'),
+            col('TPPR_TPPR_DK').alias('ID_PAI')
+        ).collect()
+    )
+
+    classes = map(
+        lambda row: row.asDict(),
+        spark.table("{}.mcpr_classe_docto_mp".format(schema_exadata)).select(
+            col('CLDC_DK').alias('ID'),
+            col('CLDC_DS_CLASSE').alias('DESCRICAO'),
+            col('CLDC_CD_CLASSE').alias('COD_MGP'),
+            col('CLDC_CLDC_DK_SUPERIOR').alias('ID_PAI')
+        ).collect()
+    )
+
+    table_name = "{}.mmps_tp_andamento".format(schema_exadata_aux)
+    create_hierarchical_table(spark, andamentos, table_name)
+    print('andamentos gravados')
+
+    table_name = "{}.mmps_classe_docto".format(schema_exadata_aux)
+    create_hierarchical_table(spark, classes, table_name)
+    print('classes gravados')
+
+    _update_impala_table(table_name, options['impala_host'], options['impala_port'])
 
 
-spark = pyspark.sql.session.SparkSession\
-        .builder\
-        .appName("tabelas_dominio")\
-        .enableHiveSupport()\
-        .getOrCreate()
+if __name__ == "__main__":
 
-sc = spark.sparkContext
+    parser = argparse.ArgumentParser(description="Create table acervo")
+    parser.add_argument('-e','--schemaExadata', metavar='schemaExadata', type=str, help='')
+    parser.add_argument('-a','--schemaExadataAux', metavar='schemaExadataAux', type=str, help='')
+    parser.add_argument('-i','--impalaHost', metavar='impalaHost', type=str, help='')
+    parser.add_argument('-o','--impalaPort', metavar='impalaPort', type=str, help='')
+    args = parser.parse_args()
 
-andamentos = map(
-    lambda row: row.asDict(),
-    spark.table("{}.mcpr_tp_andamento".format(schema_exadata)).select(
-        col('TPPR_DK').alias('ID'),
-        col('TPPR_DESCRICAO').alias('DESCRICAO'),
-        col('TPPR_CD_TP_ANDAMENTO').alias('COD_MGP'),
-        col('TPPR_TPPR_DK').alias('ID_PAI')
-    ).collect()
-)
+    options = {
+                    'schema_exadata': args.schemaExadata, 
+                    'schema_exadata_aux': args.schemaExadataAux,
+                    'impala_host' : args.impalaHost,
+                    'impala_port' : args.impalaPort
+                }
 
-classes = map(
-    lambda row: row.asDict(),
-    spark.table("{}.mcpr_classe_docto_mp".format(schema_exadata)).select(
-        col('CLDC_DK').alias('ID'),
-        col('CLDC_DS_CLASSE').alias('DESCRICAO'),
-        col('CLDC_CD_CLASSE').alias('COD_MGP'),
-        col('CLDC_CLDC_DK_SUPERIOR').alias('ID_PAI')
-    ).collect()
-)
-
-create_hierarchical_table(andamentos, 'mmps_tp_andamento')
-print('andamentos gravados')
-
-create_hierarchical_table(classes, 'mmps_classe_docto')
-print('classes gravados')
+    execute_process(options)
