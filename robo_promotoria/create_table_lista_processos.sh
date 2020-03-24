5a84e17a5700758f1055c77a11ac1bf7b10fbb8b
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-03-24 18:55:00 -0300

    Criacao da tabela que lista os processos

---
+++ robo_promotoria/create_table_lista_processos.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+export PYTHONIOENCODING=utf8
+
+spark2-submit --master yarn \
+    --queue root.mpmapas \
+    --num-executors 5 \
+    --executor-cores 1 \
+    --executor-memory 10g \
+    --conf spark.debug.maxToStringFields=2000 \
+    --conf spark.executor.memoryOverhead=4096 \
+    --conf spark.network.timeout=300 \
+    --py-files src/utils.py,packages/*.whl,packages/*.egg,packages/*.zip src/tabela_lista_processos.py $@
