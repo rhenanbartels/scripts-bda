a8b820ea8ec3e32e2ea9d121981958beef7f27d0
matteushc <matteuscunhas@gmail.com>
2020-03-24 14:56:30 -0300

    Inclusao do projeto para fazer carga de uma tabela para outra

---
+++ load_data_table/src/params_table.py
@@ -0,0 +1,12 @@
+params = {
+    "tables": [
+        {
+            "table_name": "tb_acervo",
+            "partition": "dt_partition"
+        },
+        {
+            "table_name": "tb_distribuicao",
+            "partition": "dt_partition"
+        }
+    ]
+}
