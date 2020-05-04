ab35759ca8b2c59d2627791a0f7ea9df6afe228d
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-05-04 17:53:27 -0300

    Tira flag de removido de nomes que aparecem em novos documentos

--- robo_promotoria/src/tabela_pip_investigados.py
+++ robo_promotoria/src/tabela_pip_investigados.py
@@ -1,5 +1,6 @@
 import pyspark
 from utils import _update_impala_table
+from happybase import Connection
 import argparse
 
 
@@ -14,16 +15,48 @@ def execute_process(options):
     schema_exadata = options['schema_exadata']
     schema_exadata_aux = options['schema_exadata_aux']
 
-    table = spark.sql("""
-        SELECT pess_nm_pessoa, pip_codigo, COUNT(*) as nr_investigacoes
+
+    investigados = spark.sql("""
+        SELECT pess_nm_pessoa, pip_codigo, docu_dk, docu_dt_cadastro
         FROM {0}.mcpr_personagem
         JOIN {0}.mcpr_pessoa ON pess_dk = pers_pess_dk
         JOIN {0}.mcpr_documento ON docu_dk = pers_docu_dk
-        JOIN {1}.tb_pip_aisp ON pip_codigo = docu_orgi_orga_dk_responsavel
+        JOIN {0}.mcpr_tp_personagem ON pers_tppe_dk = tppe_dk
+        JOIN (SELECT DISTINCT pip_codigo FROM {1}.tb_pip_aisp) P ON pip_codigo = docu_orgi_orga_dk_responsavel
         WHERE pers_tppe_dk IN (290, 7, 21, 317, 20, 14, 32, 345, 40, 5)
         AND docu_tpst_dk != 11
-        GROUP BY pess_nm_pessoa, pip_codigo
+        AND docu_fsdc_dk = 1
     """.format(schema_exadata, schema_exadata_aux))
+    investigados.registerTempTable('INVESTIGADOS')
+
+    table = spark.sql("""
+        SELECT pess_nm_pessoa, pip_codigo, COUNT(*) as nr_investigacoes
+        FROM INVESTIGADOS
+        GROUP BY pess_nm_pessoa, pip_codigo
+    """)
+
+    new_names = spark.sql("""
+        SELECT pip_codigo, collect_list(pess_nm_pessoa) as nomes
+        FROM INVESTIGADOS
+        WHERE to_date(docu_dt_cadastro) = to_date(current_timestamp())
+        GROUP BY pip_codigo
+    """).collect()
+
+    conn = Connection('bda1node05.pgj.rj.gov.br')
+    t = conn.table('dev:pip_investigados_flags')
+    for orgao in new_names:
+        orgao_id = str(orgao['pip_codigo'])
+        nomes = orgao['nomes']
+        removed_rows = t.scan(
+            row_prefix=orgao_id,
+            filter=(
+                # "SingleColumnValueFilter ('flags','is_removed',=,'regexstring:^True$') AND "
+                "DependentColumnFilter ('flags', 'is_removed')"
+            )
+        )
+        for row in removed_rows:
+            if row[1]['identificacao:nm_personagem'].decode('utf-8') in nomes:
+                t.delete(row[0])
 
     table_name = "{}.tb_pip_investigados".format(schema_exadata_aux)
 
