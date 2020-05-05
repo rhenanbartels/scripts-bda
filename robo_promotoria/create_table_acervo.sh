710f73f8824912e5a83aa386cb28ba2501f7b3ef
matteushc <matteuscunhas@gmail.com>
2020-05-04 21:17:12 -0300

    Ajustando parametros do spark

--- robo_promotoria/create_table_acervo.sh
+++ robo_promotoria/create_table_acervo.sh
@@ -9,4 +9,5 @@ spark2-submit --master yarn --deploy-mode cluster \
     --conf spark.debug.maxToStringFields=2000 \
     --conf spark.executor.memoryOverhead=4096 \
     --conf spark.network.timeout=300 \
+    --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35" \
     --py-files src/utils.py,packages/*.whl,packages/*.egg,packages/*.zip src/tabela_acervo.py $@
--- robo_promotoria/create_table_tempo_tramitacao.sh
+++ robo_promotoria/create_table_tempo_tramitacao.sh
@@ -11,6 +11,7 @@ spark2-submit --master yarn --deploy-mode cluster \
     --conf spark.executor.memoryOverhead=4096 \
     --conf spark.network.timeout=300 \
     --conf spark.driver.maxResultSize=6000 \
-    --conf spark.default.parallelism=100 \
-    --conf spark.sql.shuffle.partitions=100 \
+    --conf spark.default.parallelism=30 \
+    --conf spark.sql.shuffle.partitions=30 \
+    --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35" \
     --py-files src/utils.py,src/files_tempo_tramitacao.zip,packages/*.whl,packages/*.egg,packages/*.zip src/tabela_tempo_tramitacao.py $@
