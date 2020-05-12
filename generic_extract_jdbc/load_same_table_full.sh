358e34a1b114995859af0d738358f9161246ec3a
matteushc <matteuscunhas@gmail.com>
2020-05-12 17:07:11 -0300

    Ajustando parametros do spark e da partição das tabelas vinda do oracle

--- generic_extract_jdbc/load_same_table_full.sh
+++ generic_extract_jdbc/load_same_table_full.sh
@@ -4,17 +4,18 @@ export PYTHONIOENCODING=utf8
 spark2-submit --master yarn --deploy-mode cluster \
     --jars /opt/cloudera/parcels/CDH-5.14.2-1.cdh5.14.2.p0.3/jars/ojdbc6.jar,/opt/cloudera/parcels/CDH-5.14.2-1.cdh5.14.2.p0.3/jars/postgresql-9.0-801.jdbc4.jar \
     --queue root.mpmapas \
-    --num-executors 10 \
+    --num-executors 12 \
     --driver-memory 6g \
     --executor-cores 5 \
-    --executor-memory 5g \
+    --executor-memory 10g \
     --conf spark.debug.maxToStringFields=2000 \
     --conf spark.executor.memoryOverhead=4096 \
     --conf spark.network.timeout=360 \
     --conf spark.speculation=true \
-    --conf spark.speculation.quantile=0.5 \
-    --conf spark.shuffle.io.maxRetries=5 \
-    --conf spark.shuffle.io.retryWait=15s \
-    --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35 -XX:G1HeapRegionSize=16M" \
+    --conf spark.default.parallelism=150 \
+    --conf spark.sql.shuffle.partitions=100 \
+    --conf spark.locality.wait=0 \
+    --conf spark.shuffle.io.numConnectionsPerPeer=4 \
+    --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35" \
     --py-files src/*.py,packages/*.egg,packages/*.whl,packages/*.zip\
     src/generic_load_table.py $@
