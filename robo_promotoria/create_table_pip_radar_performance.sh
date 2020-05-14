2b74671b958594587e45ea488073c381ff533911
Rhenan <rhenan.bartels@gmail.com>
2020-05-14 14:06:23 -0300

    Adiciona novas configurações do spark-submit

--- robo_promotoria/create_table_pip_radar_performance.sh
+++ robo_promotoria/create_table_pip_radar_performance.sh
@@ -10,4 +10,7 @@ spark2-submit --master yarn --deploy-mode cluster \
     --conf spark.debug.maxToStringFields=2000 \
     --conf spark.executor.memoryOverhead=4096 \
     --conf spark.network.timeout=360 \
+    --conf spark.speculation=true
+    --conf spark.locality.wait=0 \
+    --conf spark.shuffle.io.numConnectionsPerPeer=3 \
     --py-files src/utils.py,packages/*.whl,packages/*.egg,packages/*.zip src/tabela_pip_radar_performance.py $@
