05f804b31afa61357dc326355b9eac5702bd55da
Rhenan <rhenan.bartels@gmail.com>
2020-05-14 14:00:45 -0300

    Atualiza spark.network.timeout

--- robo_promotoria/create_table_pip_radar_performance.sh
+++ robo_promotoria/create_table_pip_radar_performance.sh
@@ -9,6 +9,6 @@ spark2-submit --master yarn --deploy-mode cluster \
     --executor-memory 8g \
     --conf spark.debug.maxToStringFields=2000 \
     --conf spark.executor.memoryOverhead=4096 \
-    --conf spark.network.timeout=300 \
+    --conf spark.network.timeout=360 \
     --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35 -XX:G1HeapRegionSize=16M" \
     --py-files src/utils.py,packages/*.whl,packages/*.egg,packages/*.zip src/tabela_pip_radar_performance.py $@
