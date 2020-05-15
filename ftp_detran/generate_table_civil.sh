ab592513e8a8d8aea3ea725ead4b2d54eebbfc2f
matteushc <matteuscunhas@gmail.com>
2020-05-15 10:37:31 -0300

    Incluindo processo de processamento dos dados do detran

---
+++ ftp_detran/generate_table_civil.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+export PYTHONIOENCODING=utf8
+
+spark2-submit --master yarn --deploy-mode cluster \
+    --queue root.mpmapas \
+    --num-executors 3 \
+    --driver-memory 3g \
+    --executor-cores 3 \
+    --executor-memory 3g \
+    --conf spark.debug.maxToStringFields=2000 \
+    --conf spark.executor.memoryOverhead=2g \
+    --conf spark.network.timeout=360 \
+    --conf spark.speculation=true \
+    --conf spark.locality.wait=0 \
+    --conf spark.shuffle.io.numConnectionsPerPeer=2 \
+    --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35" \
+    --py-files src/*.py,packages/*.egg,packages/*.whl,packages/*.zip\
+    src/generate_table_civil.py $@
