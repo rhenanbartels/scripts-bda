d0f05df7526173ac87c5d822070d8f60d2ada25d
matteushc <matteuscunhas@gmail.com>
2020-06-02 13:29:36 -0300

    Removendo tabelas desnecessarias nos parametros

--- load_data_table/src/params_table.py
+++ load_data_table/src/params_table.py
@@ -1,14 +1,6 @@
 params = {
     "tables": [
         {
-            "table_name": "tb_acervo",
-            "partition": "dt_partition"
-        },
-        {
-            "table_name": "tb_distribuicao",
-            "partition": "dt_partition"
-        },
-        {
             "table_name": "atualizacao_pj_pacote",
             "partition": ""
         }
