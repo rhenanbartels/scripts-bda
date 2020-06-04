23497bfdb96169b51040a30af92f8cf5cc89762f
matteushc <matteus_h@hotmail.com>
2020-06-04 13:48:41 -0300

    Ajustes

--- ftp_tce/src/utils.py
+++ ftp_tce/src/utils.py
@@ -1,4 +1,37 @@
 from impala.dbapi import connect as impala_connect
+import datetime
+import requests
+import json
+import os
+import sys
+import pysolr
+
+ERROR = "ERROR"
+SUCCESS = "SUCCESS"
+SUCCESS_MESSAGE = "Processo {} executado com sucesso"
+ERROR_MESSAGE = "Erro em {}: {}"
+
+def connect_to_solr(zookeeper_server):
+    zookeeper = pysolr.ZooKeeper(zookeeper_server)
+    return pysolr.SolrCloud(zookeeper, "log_files", timeout=300)
+
+def send_data_to_solr(data, solr_server):
+    solr = connect_to_solr(solr_server)
+    solr.add(data, overwrite=True)
+
+def send_log(message, module, levelname, solr_server):
+    time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3].replace(" ", "T") + "Z"
+    date_now = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    data = [
+            {"name": os.path.basename(sys.argv[0]), 
+            "module": module, 
+            "asctime": time_stamp, 
+            "date": date_now, 
+            "message": message, 
+            "levelname": levelname
+            }]
+    send_data_to_solr(data, solr_server)
 
 def _update_impala_table(table, impalaHost, impalaPort):
     """
