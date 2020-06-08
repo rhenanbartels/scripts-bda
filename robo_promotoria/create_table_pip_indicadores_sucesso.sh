8e2e77a52db0482fd2853468d734d4bc289b154e
rhenanbartels <rhenan.bartels@gmail.com>
2020-06-08 10:17:02 -0300

    Adiciona script shell para indicadores de sucesso

---
+++ robo_promotoria/create_table_pip_indicadores_sucesso.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+export PYTHONIOENCODING=utf8
+
+spark2-submit --master yarn --deploy-mode cluster \
+    --queue root.robopromotoria \
+    --num-executors 10 \
+    --driver-memory 5g \
+    --executor-cores 5 \
+    --executor-memory 8g \
+    --conf spark.debug.maxToStringFields=2000 \
+    --conf spark.executor.memoryOverhead=4096 \
+    --conf spark.network.timeout=360 \
+    --conf spark.speculation=true \
+    --conf spark.locality.wait=0 \
+    --conf spark.shuffle.io.numConnectionsPerPeer=3 \
+    --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35" \
+    --py-files src/utils.py,packages/*.whl,packages/*.egg,packages/*.zip src/tabela_pip_indicadores_sucesso.py $@
