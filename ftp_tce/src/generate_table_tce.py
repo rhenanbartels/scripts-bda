90a48931d06a902c36ab0da9d24d5d9ae04f42ac
matteushc <matteuscunhas@gmail.com>
2020-06-09 10:15:44 -0300

    Criação do processo ftp TCE

--- ftp_tce/src/generate_table_tce.py
+++ ftp_tce/src/generate_table_tce.py
@@ -4,7 +4,6 @@ import pyspark
 from hdfs import InsecureClient
 from pyspark.sql.functions import year
 from utils import _update_impala_table, send_log, ERROR, SUCCESS, SUCCESS_MESSAGE, ERROR_MESSAGE
-#from send_request_solr import send_log, ERROR, SUCCESS, SUCCESS_MESSAGE, ERROR_MESSAGE
 
 
 def execute_process(args):
