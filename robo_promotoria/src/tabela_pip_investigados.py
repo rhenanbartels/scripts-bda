1f9c241406ce9749e4b14d9ad686e47b9c044201
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-09-10 20:03:59 -0300

    Hotfix: Resolve ambiguidade de coluna gerada pelo ultimo commit

--- robo_promotoria/src/tabela_pip_investigados.py
+++ robo_promotoria/src/tabela_pip_investigados.py
@@ -135,7 +135,7 @@ def execute_process(options):
             FROM (
                 SELECT representante_dk, COUNT(1) as nr_investigacoes
                 FROM {1}.tb_pip_investigados_procedimentos
-                JOIN {0}.mcpr_pessoa P ON pess_dk = representante_dk
+                JOIN {0}.mcpr_pessoa P ON P.pess_dk = representante_dk
                 WHERE P.pess_nm_pessoa NOT REGEXP 'IDENTIFICADO|IGNORAD[OA]|P.BLICO|JUSTI.A P.BLICA'
                 GROUP BY representante_dk
             ) c
