46d38faf845b876e57ed2d40e35df80e8ccb9f93
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-09-21 21:34:20 -0300

    Pequenos ajustes

--- classificador_desaparecimentos/run_predict.sh
+++ classificador_desaparecimentos/run_predict.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 export PYTHONIOENCODING=utf8
 
+kinit -kt /home/mpmapas/keytab/mpmapas.keytab mpmapas
+
 spark-submit --py-files packages/*.whl,packages/*.egg \
     --keytab "/home/mpmapas/keytab/mpmapas.keytab" \
     --principal mpmapas \
-    --jars /opt/cloudera/parcels/CDH-5.14.2-1.cdh5.14.2.p0.3/jars/ojdbc6.jar \
+    --jars /home/mpmapas/libs_jars/drivers_jdbc/ojdbc6.jar \
     src/predict.py $@
+
+kdestroy
