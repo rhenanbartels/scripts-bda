239f2a7750955a61402ea36fe226427c444f8741
matteushc <matteuscunhas@gmail.com>
2020-09-24 12:53:15 -0300

    Inclusao da autenticacao kerberos

--- ftp_tce/execute_generate_table_tce.sh
+++ ftp_tce/execute_generate_table_tce.sh
@@ -2,6 +2,8 @@
 export PYTHONIOENCODING=utf8
 
 spark-submit --master yarn --deploy-mode cluster \
+    --principal mpmapas \
+    --keytab "/home/mpmapas/keytab/mpmapas.keytab" \
     --jars /opt/cloudera/parcels/CDH-5.14.2-1.cdh5.14.2.p0.3/jars/ojdbc6.jar,/opt/cloudera/parcels/CDH-5.14.2-1.cdh5.14.2.p0.3/jars/postgresql-9.0-801.jdbc4.jar \
     --queue root.mpmapas \
     --num-executors 3 \
@@ -10,9 +12,11 @@ spark-submit --master yarn --deploy-mode cluster \
     --conf spark.debug.maxToStringFields=2000 \
     --conf spark.executor.memoryOverhead=4096 \
     --conf spark.network.timeout=900 \
-    --conf spark.speculation=true \
     --conf spark.shuffle.io.maxRetries=5 \
     --conf spark.shuffle.io.retryWait=15s \
+    --conf spark.executorEnv.PYTHON_EGG_CACHE="/tmp" \
+    --conf spark.executorEnv.PYTHON_EGG_DIR="/tmp" \
+    --conf spark.driverEnv.PYTHON_EGG_CACHE="/tmp" \
+    --conf spark.driverEnv.PYTHON_EGG_DIR="/tmp" \
     --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35" \
-    --keytab "/home/mpmapas/keytab/mpmapas.keytab" \
     --py-files src/*.py,packages/*.whl,packages/*.egg,packages/*.zip src/generate_table_tce.py $@
